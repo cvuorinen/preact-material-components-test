ee90d797bc4f3552acd390e0d2db316940c3f2a6
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-12-14 00:02:31 +0200

    Use Card typings in Home component

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,5 +1,5 @@
 import { h, Component } from 'preact';
-import Card from 'preact-material-components/Card';
+import Card, { Primary, Title, Subtitle, SupportingText, Actions, Action } from 'preact-material-components/Card/Card';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
 import * as style from './style.css';
@@ -10,16 +10,16 @@ export default class Home extends Component<{}, {}> {
 			<div class={style.home}>
 				<h1>Home route</h1>
 				<Card>
-					<Card.Primary>
-						<Card.Title>Home card</Card.Title>
-						<Card.Subtitle>Welcome to home route</Card.Subtitle>
-					</Card.Primary>
-					<Card.SupportingText>
+					<Primary>
+						<Title>Home card</Title>
+						<Subtitle>Welcome to home route</Subtitle>
+					</Primary>
+					<SupportingText>
 						Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-					</Card.SupportingText>
-					<Card.Actions>
-						<Card.Action>OKAY</Card.Action>
-					</Card.Actions>
+					</SupportingText>
+					<Actions>
+						<Action>OKAY</Action>
+					</Actions>
 				</Card>
 			</div>
 		);
